c4d3eff48734faf5e7b1e96818517f53da897d1d
Mako0530 <zhongtiancheng79@gmail.com>
2021-02-02 12:23:04 +0900

    投稿機能・詳細・削除機能

--- Gemfile
+++ Gemfile
@@ -25,8 +25,8 @@ gem 'annotate'
 gem 'better_errors'
 gem 'binding_of_caller'
 gem 'devise'
-
-
+gem 'carrierwave', '~> 2.0'
+gem "mini_magick"
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
---
+++ app/models/post.rb
@@ -0,0 +1,24 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :bigint           not null, primary key
+#  caption    :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_posts_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
+class Post < ApplicationRecord
+  belongs_to :user
+  has_many :photos, dependent: :destroy
+
+  accepts_nested_attributes_for :photos
+end
